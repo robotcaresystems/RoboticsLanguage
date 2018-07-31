5487dd1cbf6ffc8f0ac44ae0247e081e766e4899
Gabriel <g.lopes@robotcaresystems.com>
2018-07-31 16:53:50 +0200

    work in progress

--- RoboticsLanguage/Transformers/TypeChecking/Transform.py
+++ RoboticsLanguage/Transformers/TypeChecking/Transform.py
@@ -8,11 +8,48 @@
 #      Licence: license
 #    Copyright: copyright
 #
-
+import random
+import string
 from RoboticsLanguage.Tools import Semantic
+from RoboticsLanguage.Base import Utilities
+
+
+def isDefinition(child):
+  return random.choice([False, True])
+
+
+def updateEnvironment(environment, child):
+  print('creating new definition in: ' + child.tag)
+  data = {}
+  data.update(environment)
+  hash = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+  data[hash] = hash
+  return data
+
+
+def check(environment, code):
+  code.attrib['type'] = str(environment)
+  Utilities.printCode(code)
+  Utilities.printParameters(environment)
+
+
+def checkRecursively(environment, code):
+  new_environment = {}
+  new_environment.update(environment)
+
+  for child in code.getchildren():
+    if isDefinition(child):
+      new_environment = updateEnvironment(new_environment, child)
+
+    checkRecursively(new_environment, child)
+
+  check(environment, code)
 
 
 def transform(code, parameters):
+
+  checkRecursively({}, code)
+
 
   # check if should ignore semantic checking
   if parameters['Transformers']['TypeChecking']['ignoreSemanticErrors']:
