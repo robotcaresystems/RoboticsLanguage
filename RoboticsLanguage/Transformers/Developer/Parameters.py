deb1f1775275f415dacc442310233424d51e5d08
Gabriel <g.lopes@robotcaresystems.com>
2019-02-07 09:55:22 +0100

    automatically remove cache and no need for explicit developer output

---
+++ RoboticsLanguage/Transformers/Developer/Parameters.py
@@ -0,0 +1,14 @@
+#
+#   This is the Robotics Language compiler
+#
+#   Parameters.py: Definition of the parameters for this package
+#
+#   Created on: 07 February, 2019
+#       Author: Gabriel Lopes
+#      Licence: license
+#    Copyright: copyright
+#
+
+parameters = {}
+
+command_line_flags = {}
