ca9ec9de480a2e191843c9929ec7224e15a9becc
Gabriel <g.lopes@robotcaresystems.com>
2019-04-03 16:10:37 +0200

    associative arrays

---
+++ RoboticsLanguage/Transformers/AssociativeArray/Manifesto.py
@@ -0,0 +1,45 @@
+#
+#   This is the Robotics Language compiler
+#
+#   Manifesto.py: Definition of the manifesto for this package
+#
+#   Created on: 03 April, 2019
+#       Author: Gabriel Lopes
+#      Licence: Apache 2.0
+#    Copyright: Robots Robots Company B.V.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+#   this file except in compliance with the License. You may obtain a copy of the
+#   License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+#   applicable law or agreed to in writing, software distributed under the License
+#   is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#   KIND, either express or implied. See the License for the specific language
+#   governing permissions and limitations under the License.
+#
+manifesto = {
+  'packageName': 'Associative Array',
+  'packageShortName': 'AssociativeArray',
+  'order': 100,
+  'version': '0.0.0',
+  
+  'information': {
+    'author':
+    {
+      'name': 'Gabriel Lopes',
+      'email': 'g.lopes@rrcrobotics.com',
+      'web': 'http://www.dcsc.tudelft.nl/~glopes/publications.html',
+      'telephone': 'user telephone'
+    },
+    'company':
+    {
+      'name': 'Robots Robots Company B.V.',
+      'address': 'Rotterdamseweg 402P',
+      'zipcode': '2629HH',
+      'city': 'Delft',
+      'country': 'Netherlands',
+      'email': 'info@rrcrobotics.com',
+      'web': 'http://www.rrcrobotics.com',
+      'telephone': '+31 85 130 56 70',
+    }
+  }
+}
---
+++ RoboticsLanguage/Transformers/AssociativeArray/README.md
@@ -0,0 +1,9 @@
+# Transformer module Associative Array
+
+The Associative Array Transformers implements ...
+
+
+ - [Documentation](Documentation/README.md)
+ - [Tutorials](Documentation/Tutorials.md)
+ - [Reference](Documentation/Reference.md)
+ - [Examples](Examples)
