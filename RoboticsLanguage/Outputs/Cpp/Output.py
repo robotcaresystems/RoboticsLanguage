b6336f486c3b043f860487a5316b224d2be9185c
Gabriel Alexandre Delgado Lopes <glopes@Gabriels-MBP.caspar.ai>
2019-11-19 14:21:12 +0100

    working for simple examples

--- RoboticsLanguage/Outputs/Cpp/Output.py
+++ RoboticsLanguage/Outputs/Cpp/Output.py
@@ -9,16 +9,56 @@
 #    Copyright: copyright
 #
 
-
+import os
 import sys
+import subprocess
 from RoboticsLanguage.Base import Utilities
 from RoboticsLanguage.Tools import Templates
 
 def output(code, parameters):
-  Utilities.logging.info("Output Cpp...")
 
-  # run template engine to generate code
-  if not Templates.templateEngine(code, parameters):
+  # save the node name for the templates
+  parameters['node']['name'] = code.xpath('/node/option[@name="name"]/string')[0].text
+
+  # find a file system safe name
+  node_name_underscore = Utilities.underscore(parameters['node']['name'])
+
+  deploy_path = parameters['globals']['deploy']
+
+  # ############ generate code #####################################################
+
+  # run template engine to generate node code
+  if not Templates.templateEngine(code, parameters, file_patterns={'nodename': node_name_underscore}):
     sys.exit(1)
+
+  # ############ compile #####################################################
+
+  # if the flag compile is set then run catkin
+  if parameters['globals']['compile']:
+    command = ['make']
+    folder = deploy_path + '/' + node_name_underscore
+
+    Utilities.logging.debug("Compiling with: `" + ' '.join(command) + "` in folder " + folder)
+    process = subprocess.Popen(command, cwd=folder)
+    process.wait()
+
+    if process.returncode > 0:
+      Utilities.logging.error("Compilation failed!!!")
+
+  # ############ run code #####################################################
+  # if the flag launch is set then launch the node
+  if parameters['globals']['launch']:
+
+    command = ['./' + node_name_underscore]
+    folder = os.path.join(deploy_path , node_name_underscore , 'build')
+
+    Utilities.logging.debug("Running command `" + ' '.join(command) + "` in folder " + folder)
+    try:
+      process = subprocess.Popen(command, cwd=folder)
+      process.wait()
+    except Exception as e:
+      print(e)
+
+
 
   return 0
--- RoboticsLanguage/Outputs/Python/Output.py
+++ RoboticsLanguage/Outputs/Python/Output.py
@@ -9,13 +9,19 @@
 #    Copyright: copyright
 #
 
-
+import os
 import sys
+import stat
+import autopep8
 from RoboticsLanguage.Base import Utilities
 from RoboticsLanguage.Tools import Templates
 
 
 def output(code, parameters):
+
+  deploy_path = parameters['globals']['deploy']
+
+  # ############ code generation #####################################################
 
   # save the node name for the templates
   parameters['node']['name'] = code.xpath('/node/option[@name="name"]/string')[0].text
@@ -26,5 +32,41 @@ def output(code, parameters):
   # run template engine to generate code
   if not Templates.templateEngine(code, parameters, file_patterns={'nodename': node_name_underscore}):
     sys.exit(1)
+
+  # ############ Indentation #####################################################
+  # Make sure indentation is respected
+  python_file = deploy_path + '/' + node_name_underscore + '/scripts/' + node_name_underscore + '.py'
+
+  # show indentation marks
+  if parameters['Outputs']['RosPy']['showPythonIndentationMarks']:
+    with open(python_file, 'r') as file:
+      Utilities.printSource(file.read(), 'python', parameters)
+
+  # precess indentation marks
+  indent = 0
+  indent_step = 4
+  python_text = ''
+  with open(python_file, 'r') as file:
+    for line in file:
+      clean_line = line.strip()
+      if clean_line == '#>>':
+        indent = indent + indent_step
+        continue
+      if clean_line == '#<<':
+        indent = indent - indent_step
+        continue
+      python_text += ' '*indent + clean_line + '\n'
+
+  # beautify
+  if parameters['globals']['beautify']:
+    python_text = autopep8.fix_code(python_text)
+
+  # save the main script
+  with open(python_file, 'w') as file:
+    file.write(python_text)
+
+  # make sure the python script is executable
+  st = os.stat(python_file)
+  os.chmod(python_file, st.st_mode | stat.S_IEXEC)
 
   return 0
